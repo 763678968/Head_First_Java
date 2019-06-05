d6c29460084af667597fab9738602b9f17ab743f
Animation <763678968@qq.com>
2019-06-05 22:39:22 +0800

    Duck

---
+++ src/Duck.java
@@ -0,0 +1,15 @@
+public class Duck {
+    private int size;
+
+    public static void main(String[] args) {
+        System.out.println("Size is " + getSize());
+    }
+
+    public void setSize(int s) {
+        size = s;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
