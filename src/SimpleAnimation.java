37d717ee8a8eb7d40726a157bfbee06cad7edde0
Animation <763678968@qq.com>
2019-06-09 19:59:30 +0800

    SimpleAnimation

--- src/SimpleAnimation.java
+++ src/SimpleAnimation.java
@@ -36,6 +36,9 @@ public class SimpleAnimation {
 
     class MyDrawPanel extends JPanel {
         public void paintComponent(Graphics g) {
+            g.setColor(Color.white);
+            g.fillRect(0,0,this.getWidth(), this.getHeight());
+
             g.setColor(Color.green);
             g.fillOval(x,y,40,40);
         }
