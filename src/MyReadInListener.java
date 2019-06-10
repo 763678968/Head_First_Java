64b8497261e9db069a10c7a0b63473848729c853
Animation <763678968@qq.com>
2019-06-10 21:07:18 +0800

    MyReadInListener

---
+++ src/MyReadInListener.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.sound.midi.*;
+import java.io.*;
+import java.util.*;
+import java.awt.event.*;
+
+public class MyReadInListener implements ActionListener {
+
+    public void actionPerformed(ActionEvent a) {
+        boolean[] checkboxState = null;
+        try {
+            FileInputStream fileIn = new FileInputStream(new File("Checkbox.ser"));
+            ObjectInputStream is = new ObjectInputStream(fileIn);
+            checkboxState = (boolean[]) is.readObject();
+
+        } catch(Exception ex) {ex.printStackTrace();}
+
+        for (int i = 0; i < 256; i++) {
+            JCheckBox check = (JCheckBox) checkboxList.get(i);
+            if (checkboxState[i]) {
+                check.setSelected(true);
+            } else {
+                check.setSelected(false);
+            }
+        }
+
+        sequencer.stop();
+        buildTrackAndStart();
+
+    } // 关闭方法
+} // 关闭内部类
