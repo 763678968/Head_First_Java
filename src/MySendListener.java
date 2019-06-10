75e24849dacb9571e2583fca6e71371b6c3de7a9
Animation <763678968@qq.com>
2019-06-10 21:02:13 +0800

    MySendListener

---
+++ src/MySendListener.java
@@ -0,0 +1,31 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.sound.midi.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.*;
+import java.awt.event.*;
+
+public class MySendListener implements ActionListener {
+
+    public void actionPerformed(ActionEvent a) {
+        boolean[] checkboxState = new boolean[256];
+
+        for (int i = 0; i < 256; i++) {
+
+            JCheckBox check = (JCheckBox) checkboxList.get(i);
+            if (check.isSelected()) {
+                checkboxState[i] = true;
+            }
+        }
+
+        try {
+            FileOutputStream fileStream = new FileOutputStream(new File("Checkbox.ser"));
+            ObjectOutputStream os = new ObjectOutputStream(fileStream);
+            os.writeObject(checkboxState);
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    } // 关闭方法
+} // 关闭内部类
