c62103426ed168189e3c380256d542ec45e086ee
Animation <763678968@qq.com>
2019-06-12 16:25:07 +0800

    TestTree_2

--- src/TestTree.java
+++ src/TestTree.java
@@ -18,9 +18,13 @@ public class TestTree {
     }
 }
 
-class Book {
+class Book implements Comparable {
     String title;
     public Book(String t) {
         title = t;
+    }
+    public int compareTo(Object b) {
+        Book book = (Book) b;
+        return (title.compareTo(book.title));
     }
 }
