e4ec7eac12a3908e50aa09737256dead6a9b17f4
animation <763678968@qq.com>
2019-09-10 19:55:45 +0800

    HelloWorld.java

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public void sayHellO() {
+        System.out.println("hello java");
+    }
+    public void sayHelloToPHP() {
+        System.out.println("hello php");
+    }
+}
