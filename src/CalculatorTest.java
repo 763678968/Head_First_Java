8b60c7b2c2f1b0665a403c541f0ed81c7190224e
animation <763678968@qq.com>
2019-09-10 21:11:28 +0800

    使用@Test注解

---
+++ src/CalculatorTest.java
@@ -0,0 +1,11 @@
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        System.out.println("test add method ");
+    }
+
+    @Test(ignore=true)
+    public void testSubtract() {
+        System.out.println("test subtract method ");
+    }
+}
