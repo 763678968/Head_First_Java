032fac1c50ec50c5778d1a77fbbab7bc5effa3bc
animation <763678968@qq.com>
2019-09-10 19:58:13 +0800

    经过修改的HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,12 @@
-public class HelloWorld {
-    public void sayHellO() {
-        System.out.println("hello java");
-    }
-    public void sayHelloToPHP() {
-        System.out.println("hello php");
-    }
-}
+//import java.util.logging.Logger;
+//
+//public class HelloWorld {
+//    public void sayHellO() {
+//        Logger.startlog();
+//        System.out.println("hello java");
+//        Logger.endlog();
+//    }
+//    public void sayHelloToPython() {
+//        System.out.println("hello Python");
+//    }
+//}
