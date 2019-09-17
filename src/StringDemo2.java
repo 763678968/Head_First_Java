752f4f8e530fda817a02bdefb6c26f5ec10115d2
animation <763678968@qq.com>
2019-09-17 21:20:12 +0800

    判断一个字符串中，某个子字符串出现的次数

--- src/StringDemo2.java
+++ src/StringDemo2.java
@@ -13,7 +13,20 @@ public class StringDemo2 {
         return count;
     }
 
+    // input: helloworld "low" 判断一个字符串中，某个子字符串出现的次数
+    public static int test04(String input, String word) {
+//        int position = input.indexOf(word);
+//        System.out.println(position);
+        int count = 0;
+        while(input.indexOf(word) != -1) {
+            count++;
+            input = input.substring(input.indexOf(word) + 1);
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
-        System.out.println(test03("helloworld", "o"));
+//        System.out.println(test03("helloworld", "o"));
+        System.out.println(test04("helloworldllo", "llo"));
     }
 }
