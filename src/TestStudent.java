391946b0d24078adefc22aa1bf232732710aada6
animation <763678968@qq.com>
2019-09-17 16:00:05 +0800

    TestStudent

---
+++ src/TestStudent.java
@@ -0,0 +1,49 @@
+public class TestStudent {
+
+    public static void compareStudent(Student[] students) {
+        // 冒泡排序
+        for (int i = 0; i < students.length - 1; i++) {
+            for (int j = 0; j < students.length - 1 - i; j++) {
+                if(students[j].getSum() < students[j+1].getSum()) {
+                    // 交换 int
+                    Student temp = students[j];
+                    students[j] = students[j+1];
+                    students[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Student stu1 = new Student();
+        stu1.name = "zs";
+        stu1.javaScore = 99;
+        stu1.sqlScore = 98;
+
+        Student stu2 = new Student();
+        stu2.name = "ls";
+        stu2.javaScore = 95;
+        stu2.sqlScore = 96;
+
+        Student stu3 = new Student();
+        stu3.name = "ww";
+        stu3.javaScore = 92;
+        stu3.sqlScore = 90;
+
+        double stu1Avg = stu1.getAvg();
+        double stu2Avg = stu2.getAvg();
+        double stu3Avg = stu3.getAvg();
+
+        System.out.println(stu1.name + "\t" + stu1.getSum() + "\t" + stu1.getAvg());
+        System.out.println(stu2.name + "\t" + stu2.getSum() + "\t" + stu2.getAvg());
+        System.out.println(stu3.name + "\t" + stu3.getSum() + "\t" + stu3.getAvg());
+
+        Student[] students = new Student[]{stu1, stu2, stu3};
+        compareStudent(students);
+
+        for (Student student : students) {
+            System.out.println(student.name + "\t" + student.getAvg() + "\t" + student.getSum());
+        }
+    }
+
+}
