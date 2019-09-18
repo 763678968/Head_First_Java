298481257d96013fd822b936f60af6ebfa1032b5
animation <763678968@qq.com>
2019-09-18 15:57:03 +0800

    抽象类

---
+++ src/pet/A.java
@@ -0,0 +1,16 @@
+//package pet;
+//
+//public class A {
+//    public void method() {
+//
+//    }
+//}
+//
+//class B extends A {
+//    final int num = 10;
+//
+//    @Override
+//    public void method() {
+//        num = num + 1;
+//    }
+//}
