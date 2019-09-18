05eb485133ec9fb192f7a1d4c5d72a594f46d64f
animation <763678968@qq.com>
2019-09-18 11:08:24 +0800

    方法重载

---
+++ src/TestMath.java
@@ -0,0 +1,22 @@
+class Math {
+
+    public void add(int num1, int num2) {
+        System.out.println(num1 + num2);
+    }
+
+    public void add(double num1, int num2) {
+        System.out.println(num1 + num2);
+    }
+
+    public void add(int num1, double num2) {
+        System.out.println(num1 + num2);
+    }
+
+    public void add(int num1, int num2, int num3) {
+        System.out.println(num1 + num2 + num3);
+    }
+
+}
+
+public class TestMath {
+}
