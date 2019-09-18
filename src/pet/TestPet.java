0f11ce7125ed7fa0ad907703d1fc69857055ea16
animation <763678968@qq.com>
2019-09-18 10:26:04 +0800

    构造方法 this super TestPet

--- src/pet/TestPet.java
+++ src/pet/TestPet.java
@@ -4,10 +4,13 @@ public class TestPet {
     public static void main(String[] args) {
 
         Dog dog = new Dog();
-        dog.setName("wc");
-        dog.setLove(99);
-        dog.setHealth(98);
-        dog.setStrain("哈士奇");
-        dog.print();
+
+//        Dog dog = new Dog("哈士奇", "123");
+//        dog.print();
+//        dog.setName("wc");
+//        dog.setLove(99);
+//        dog.setHealth(98);
+//        dog.setStrain("哈士奇");
+//        dog.print();
     }
 }
