4e5787924ea27354ae4a6534aca041c59f504679
animation <763678968@qq.com>
2019-09-19 11:04:03 +0800

    Socket传文件 MyClient

--- src/socket/MyClient.java
+++ src/socket/MyClient.java
@@ -1,5 +1,6 @@
 package socket;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -12,17 +13,28 @@ public class MyClient {
 
         // 接收服务端发送的消息 InputStream
         InputStream in = socket.getInputStream();
-        byte[] bs = new byte[100];
-        in.read(bs); // 读取发送来的数据
-        // byte[] -> String
-        System.out.println("client接收到的消息为：" + new String(bs));
 
-        // 客户端向服务端作出反馈（向服务端发送消息）
-        OutputStream out = socket.getOutputStream();
-        out.write("world".getBytes());
+        /* 接收普通文字hello
+        * byte[] bs = new byte[100];
+        * in.read(bs); // 读取发送来的数据
+        * byte[] -> String
+        * System.out.println("client接收到的消息为：" + new String(bs));
+        * */
 
+        byte[] bs = new byte[100]; // 接收每次发来的文件切片(100byte)
+        int len = -1;
+        OutputStream fileOut = new FileOutputStream("D:\\3.jpg");
+        while ((len = in.read(bs)) != -1) {
+            fileOut.write(bs, 0, len);
+        }
+
+//        // 客户端向服务端作出反馈（向服务端发送消息）
+//        OutputStream out = socket.getOutputStream();
+//        out.write("world".getBytes());
+
+        fileOut.close();
         in.close();
-        out.close();
+//        out.close();
         socket.close();
     }
 }
