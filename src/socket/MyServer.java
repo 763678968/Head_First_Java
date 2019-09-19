a86dbbc19dcdede3feaabbfc204224a47f5f83ba
animation <763678968@qq.com>
2019-09-19 15:36:27 +0800

    Socket文件优化 加入多线程 MyServer

--- src/socket/MyServer.java
+++ src/socket/MyServer.java
@@ -9,40 +9,17 @@ public class MyServer {
         // 绑定服务的端口，ip：为本机IP
         // 暴露了一个服务，该服务的地址：本机IP，9999
         ServerSocket server = new ServerSocket(9999);
-        Socket socket = server.accept();
-        System.out.println("与客户端连接成功！");
+        while (true) {
+            Socket socket = server.accept();
 
-        // 服务端向客户端发送消息Output
-        OutputStream out = socket.getOutputStream();
+//            // 下载的线程
+//            MyDownload download = new MyDownload(socket);
+//            // Runnable -> Thread
+//            Thread downLoadThread = new Thread(download);
+//            downLoadThread.start();
 
-        /*发送文字hello
-        * String info = "hello";
-        * String -> byte[]
-        * out.write(info.getBytes());
-        * */
-
-        // 准备要发送的文件
-        File file = new File("C:\\Users\\animation\\Desktop\\3.jpg");
-        // 将此文件从硬盘读入到内存
-        FileInputStream fileIn = new FileInputStream(file);
-        byte[] fileBytes = new byte[100]; // 定义每次发送的文件大小
-        // 发送（因为文件较大，不能一次发送完毕，因此需要通过循环来分次发送）
-        int len = -1;
-        while ((len = fileIn.read(fileBytes)) != -1) {
-            out.write(fileBytes, 0, len);
+            // 上述3行语句等价于如下语句
+            new Thread(new MyDownload((socket))).start();
         }
-
-
-//        // 接收客户端的消息
-//        InputStream in = socket.getInputStream();
-//        byte[] bs = new byte[100];
-//        in.read(bs);
-//        System.out.println("接收客户端发来的消息为：" + new String(bs));
-
-        fileIn.close();
-        out.close();
-//        in.close();
-        socket.close();
-        server.close();
     }
 }
