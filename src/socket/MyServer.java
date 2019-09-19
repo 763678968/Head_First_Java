d33ce128bf955e47d9141fb5b447255185d5cecc
animation <763678968@qq.com>
2019-09-19 11:04:13 +0800

    Socket传文件 MyServer

--- src/socket/MyServer.java
+++ src/socket/MyServer.java
@@ -1,8 +1,6 @@
 package socket;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -16,18 +14,34 @@ public class MyServer {
 
         // 服务端向客户端发送消息Output
         OutputStream out = socket.getOutputStream();
-        String info = "hello";
-        // String -> byte[]
-        out.write(info.getBytes());
 
-        // 接收客户端的消息
-        InputStream in = socket.getInputStream();
-        byte[] bs = new byte[100];
-        in.read(bs);
-        System.out.println("接收客户端发来的消息为：" + new String(bs));
+        /*发送文字hello
+        * String info = "hello";
+        * String -> byte[]
+        * out.write(info.getBytes());
+        * */
 
+        // 准备要发送的文件
+        File file = new File("C:\\Users\\animation\\Desktop\\3.jpg");
+        // 将此文件从硬盘读入到内存
+        FileInputStream fileIn = new FileInputStream(file);
+        byte[] fileBytes = new byte[100]; // 定义每次发送的文件大小
+        // 发送（因为文件较大，不能一次发送完毕，因此需要通过循环来分次发送）
+        int len = -1;
+        while ((len = fileIn.read(fileBytes)) != -1) {
+            out.write(fileBytes, 0, len);
+        }
+
+
+//        // 接收客户端的消息
+//        InputStream in = socket.getInputStream();
+//        byte[] bs = new byte[100];
+//        in.read(bs);
+//        System.out.println("接收客户端发来的消息为：" + new String(bs));
+
+        fileIn.close();
         out.close();
-        in.close();
+//        in.close();
         socket.close();
         server.close();
     }
