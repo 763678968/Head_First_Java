4df142374cc6d010fceeb0c7c2080695ac9c1e73
animation <763678968@qq.com>
2019-09-19 22:50:12 +0800

    RPC Hello实现类

---
+++ src/remote/procedure/call/server/HelloServiceImpl.java
@@ -0,0 +1,8 @@
+package remote.procedure.call.server;
+
+public class HelloServiceImpl implements HelloService {
+    @Override
+    public String sayHi(String name) {
+        return "hi," + name;
+    }
+}
