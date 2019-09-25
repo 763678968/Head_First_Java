0b541b9dd942545e68cd9d6de959efc258b8180c
animation <763678968@qq.com>
2019-09-25 22:32:19 +0800

    toString

---
+++ Thinking_in_Java/src/Test2.java
@@ -0,0 +1,8 @@
+public class Test2 {
+    public static void main(String[] args) {
+        String str = new String("WWW.RUNOOB.COM");
+
+        System.out.print("返回值 ：");
+        System.out.println(str.toString());
+    }
+}
