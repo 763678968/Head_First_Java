af6a007a12c8e9b7f93c8d47113f0c0ff4416d7a
animation <763678968@qq.com>
2019-09-27 23:32:41 +0800

    Test1

--- Thinking_in_Java/src/generics/Test1.java
+++ Thinking_in_Java/src/generics/Test1.java
@@ -1,14 +1,12 @@
 package generics;
 
-import org.apache.commons.logging.Log;
-
 public class Test1 {
-    public static void showKeyValue1(Generic<Number> obj) {
-        System.out.println("泛型测试" + "key value is" + obj.getKey());
+    public static void showKeyValue1(Generic<?> obj) {
+        System.out.println("泛型测试 " + "key value is: " + obj.getKey());
     }
 
-    Generic<Integer> gInteger = new Generic<Integer>(123);
-    Generic<Number> gNumber = new Generic<Number>(456);
+    static Generic<Integer> gInteger = new Generic<Integer>(123);
+    static Generic<Number> gNumber = new Generic<Number>(456);
 
     public static void main(String[] args) {
         showKeyValue1(gNumber);
